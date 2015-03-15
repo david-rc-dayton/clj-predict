4485285ec6357c48dbd2ec05c5062e00824eb55a
David RC Dayton <david.rc.dayton@gmail.com>
2015-03-15 12:59:22 -0600

    added license and url

--- project.clj
+++ project.clj
@@ -1,5 +1,9 @@
 (defproject clj-predict "0.0.1-SNAPSHOT"
   :description "Library for satellite propagation and other space maths."
+  :url "https://github.com/david-rc-dayton/clj-predict"
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo}
   :resource-paths ["lib/predict4java-1.1.158.4.jar"
                    "lib/commons-lang-2.6.jar"]
   :dependencies [[org.clojure/clojure "1.6.0"]]
