786c60c2fb06f131e727c5c12d667cca7f8e08c3
David RC Dayton <david.rc.dayton@gmail.com>
2015-03-18 12:13:17 -0600

    moved libs to local maven repo

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
 (defproject clj-predict "0.0.1-SNAPSHOT"
   :description "Library for satellite propagation and other space maths."
   :url "https://github.com/david-rc-dayton/clj-predict"
+  :repositories {"local" "file:lib"}
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
-  :resource-paths ["lib/predict4java-1.1.158.4.jar"
-                   "lib/commons-lang-2.6.jar"]
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :aot :all
-  :omit-souce true)
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [commons-lang "2.6"]
+                 [predict4java "1.1.158.4"]])
