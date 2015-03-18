d71f3a6ec86ee865d2b99021fc8839b73ceac62f
David RC Dayton <david.rc.dayton@gmail.com>
2015-03-18 12:33:41 -0600

    all libs pulled from external repos

--- project.clj
+++ project.clj
@@ -1,10 +1,8 @@
 (defproject clj-predict "0.0.1-SNAPSHOT"
   :description "Library for satellite propagation and other space maths."
   :url "https://github.com/david-rc-dayton/clj-predict"
-  :repositories {"local" "file:lib"}
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [commons-lang "2.6"]
-                 [predict4java "1.1.158.4"]])
+                 [uk.me.g4dpz/predict4java "1.1.3"]])
