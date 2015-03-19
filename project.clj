9344096064e4952c1cadff04af243b0c1d3bece8
David RC Dayton <david.rc.dayton@gmail.com>
2015-03-19 13:12:20 -0600

    added codox to plugins

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
             :url "http://opensource.org/licenses/MIT"
             :distribution :repo}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [uk.me.g4dpz/predict4java "1.1.3"]])
+                 [uk.me.g4dpz/predict4java "1.1.3"]]
+  :profiles {:dev {:plugins [[codox "0.8.11"]]}})
