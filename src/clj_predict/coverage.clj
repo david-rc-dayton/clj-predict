58653726f931c298cf2c2298f9c2006c6187009e
David RC Dayton <david.rc.dayton@gmail.com>
2015-04-05 20:51:16 -0600

    added function to generate blank coverage matrix

--- src/clj_predict/coverage.clj
+++ src/clj_predict/coverage.clj
@@ -1,6 +1,20 @@
 (ns clj-predict.coverage
   "Matrix generation functions for satellite coverage analysis.")
 
+(defn coordinate-matrix
+  "Generate a blank coverage matrix. Takes the argument:
+
+   > `dimensions` - a vector of the `width` & `height` for the desired matrix
+
+   Returns a matrix containing coordinates of the form `[latitude longitude]`,
+   in degrees."
+  [[width height :as dimensions]]
+  (let [step-width (double (/ 360 width))
+        step-height (- (double (/ 180 height)))]
+    (partition width (for [lat (range 90.0 -90.0 step-height)
+                           lon (range -180.0 180.0 step-width)]
+                       [lat lon]))))
+
 (defn combine-matrix
   "Add matrices `a` and `b`. A matrix is entered as a two-dimensional nested
    vector; the number of rows and columns in `a` and `b` must be equal.
