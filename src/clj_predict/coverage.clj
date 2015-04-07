58cfee874e7f9168e843b1914c46c053692e4745
David RC Dayton <david.rc.dayton@gmail.com>
2015-04-06 22:29:01 -0600

    added more coverage functionality

--- src/clj_predict/coverage.clj
+++ src/clj_predict/coverage.clj
@@ -1,19 +1,60 @@
 (ns clj-predict.coverage
   "Matrix generation functions for satellite coverage analysis.")
 
-(defn coordinate-matrix
+(def pi (Math/PI))
+(def two-pi (* 2 pi))
+(def half-pi (/ pi 2))
+
+(def ascii-legend
+  {0 ". " 1 "- " 2 "= "
+   3 "% " 4 "+ " 5 "V "
+   :default "@ "})
+
+(defn print-matrix
+  [m]
+  (let [replace-fn #(or (get ascii-legend %) (:default ascii-legend))
+        str-fn #(apply str (map replace-fn %))]
+    (dorun (map #(println (str-fn %)) m))))
+
+(defn rangef
+  [start end step]
+  (map #(+ start (* % step))
+       (range 0 (/ (Math/abs (- end start))
+                   (Math/abs step)) 1)))
+
+(defn cov-cosine
+  [^double phi-1 ^double lam-1 ^double phi-2 ^double lam-2]
+  (let [delta-phi-two (-> (Math/sin (/ (- phi-2 phi-1) 2)) (Math/pow 2))
+        delta-lam-two (-> (Math/sin (/ (- lam-2 lam-1) 2)) (Math/pow 2))
+        a (+ delta-phi-two
+             (* (Math/cos phi-1) (Math/cos phi-2) delta-lam-two))]
+    (* 2 (Math/atan2 (Math/sqrt a) (Math/sqrt (- 1 a))))))
+
+(def cov-methods
+  {:cosine cov-cosine})
+
+(defn view-fn
+  [method {:keys [^double lat ^double lon ^double alt]}]
+  (fn [gnd-lat gnd-lon]
+    (let [sat-lat (* lat (/ pi 180))
+          sat-lon (* lon (/ pi 180))
+          view-limit (Math/acos (/ 6371000 (+ 6371000 alt)))
+          view-method (get cov-methods method)]
+      (if (<= (view-method sat-lat sat-lon gnd-lat gnd-lon) view-limit)
+        1 0))))
+
+(defn blank-matrix
   "Generate a blank coverage matrix. Takes the argument:
 
    > `dimensions` - a vector of the `width` & `height` for the desired matrix
 
-   Returns a matrix containing coordinates of the form `[latitude longitude]`,
-   in degrees."
+   Returns a matrix containing coordinates of the form
+   `[latitude [longitudes]]`, in radians."
   [[width height :as dimensions]]
-  (let [step-width (double (/ 360 width))
-        step-height (- (double (/ 180 height)))]
-    (partition width (for [lat (range 90.0 -90.0 step-height)
-                           lon (range -180.0 180.0 step-width)]
-                       [lat lon]))))
+  (let [step-width (/ two-pi width)
+        step-height (- (/ pi height))
+        lon (rangef (- pi) pi step-width)]
+    (for [lat (rangef half-pi (- half-pi) step-height)] [lat lon])))
 
 (defn combine-matrix
   "Add matrices `a` and `b`. A matrix is entered as a two-dimensional nested
@@ -27,3 +68,21 @@
       (let [a-line (nth a n)
             b-line (nth b n)]
         (recur (inc n) (conj output (vec (map + a-line b-line))))))))
+
+(defn coverage-matrix
+  [method {:keys [lat lon alt] :as satellite} [width height :as dimensions]]
+  (let [matrix (blank-matrix dimensions)
+        in-view? (view-fn method satellite)]
+    (loop [m matrix o []]
+      (if (empty? m)
+        o
+        (let [row  (first m)
+              phi  (first row)
+              lams (second row)
+              rep  (map (partial in-view? phi) lams)]
+          (recur (rest m) (conj o rep)))))))
+
+(defn coverage-combined
+  [method satellites [width height :as dimensions]]
+  (let [cov-fn #(coverage-matrix method % dimensions)]
+    (reduce combine-matrix (map cov-fn satellites))))
